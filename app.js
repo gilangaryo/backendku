dbcb5d41c36d2855f2b02432c5dcc6b4be16a986
gilangaryo <gilang91668@gmail.com>
2024-01-13 01:02:54 +0700

    categories

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllcategory, getCategory, getSubCategory, getSubMenu, addSubcategory } = require('../controllers/categoryController');
+const { getAllcategory, getCategory, getSubCategory, getSubMenu, addSubcategory } = require('./controllers/categoryController');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     const categories = getAllcategory();
-    res.json(categories);  // Adjust this based on the actual response from getAllcategory()
+    res.json(categories);
 });
 
 app.listen(PORT, () => {
