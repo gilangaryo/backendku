e964b1de3dc06aff9889bf554e1ec93124acdae9
gilangaryo <gilang91668@gmail.com>
2024-01-12 19:58:56 +0700

    ternyata typo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const route = require('./routes/router');
+const router = require('./routes/router');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('', route);
+app.use('/', router);
 
 const port = 8000;
 app.listen(port, () => console.log("Up & Running on port ", port));
